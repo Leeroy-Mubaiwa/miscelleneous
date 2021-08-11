b6c5d7267a3b878b6302b2bbefc6f392cdb7ca07
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-08-11 03:32:09 +0200

    cxc

---
+++ Data Structures/DataStructures/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     for(int a =0; a<10;++a)
+   {
+       System.out.println(a);
+   }
+    }
+}
+
+class LinkedList{
+    
+}
---
+++ linked-lists.java
@@ -0,0 +1,5 @@
+public class linked-lists {
+ public static void main(String[] args) {
+     
+    }
+}
